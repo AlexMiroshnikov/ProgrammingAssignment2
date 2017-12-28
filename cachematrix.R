45b31ff42e77ca4fefba910894c2297c05a3160b
Alexey Miroshnikov <miroshnikov@CLI100343>
2017-12-28 11:06:15 +0100

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## See functions descriptions for overall description
 
-## Write a short comment describing this function
+## Returns a cache wrapper for a matrix
+## @arg x matrix
+## @return list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    set <- function(m) {
+        x <<- m
+        inv <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setInv <- function(solution) {
+        inv <<- solution
+    }
+    
+    getInv <- function() {
+        inv
+    }
+    
+    list(
+        set = set,
+        get = get,
+        setInv = setInv,
+        getInv = getInv
+    )
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse matrix for a cache-wrapped matrix x
+## @arg x cache-wrapped matrix, @see makeCacheMatrix()
+## @arg ... <args for solve()>
+## @return matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    solution <- x$getInv()
+    
+    if (!is.null(solution)) {
+        message('cache hit')
+        return(solution)
+    }
+    
+    matrix <- x$get()
+    solution <- solve(matrix, ...)
+    x$setInv(solution)
+    solution
 }
+
+## Uncomment the following code to check the functionality
+#
+#m <- matrix(1:4, 2, 2)
+#cm <- makeCacheMatrix(m)
+#result <- cacheSolve(cm)
+#result <- cacheSolve(cm)
+#
+#m <- matrix(c(1,2,0, 0,4,5, 0,2,3), nrow=3, ncol=3)
+#cm <- makeCacheMatrix(m)
+#result <- cacheSolve(cm)
+#result <- cacheSolve(cm)
